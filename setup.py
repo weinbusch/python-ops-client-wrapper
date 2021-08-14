c536bd20f2008f5aad88d88d9672cbb2bc906100
Martin Bierbaum <>
2021-08-14 16:13:44 +0200

    add dogpile.cache to setup requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    install_requires=["python_epo_ops_client"],
+    install_requires=[
+        "python_epo_ops_client~=3.1.3",
+        "dogpile.cache~=1.1.3",
+    ],
 )
